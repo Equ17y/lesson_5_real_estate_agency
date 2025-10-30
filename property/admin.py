782faf5b281899b18405bb7877cc5306dcd225c5
Equ17y <antonsobol28@gmail.com>
2025-10-30 22:31:03 +0300

    обновил режим администратора

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from property.models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'name')
+    search_fields = ('town', 'address', 'owner')
 
 admin.site.register(Flat, FlatAdmin)
